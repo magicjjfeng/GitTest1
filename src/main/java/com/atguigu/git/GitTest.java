a5392b058d7648b9d848244c97e89d7499ee69ea
magicjjfeng <1963487815@qq.com>
2021-12-02 17:25:08 +0800

    this is first

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author FJJ
+ * @creat 2021--12--02--17:21
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

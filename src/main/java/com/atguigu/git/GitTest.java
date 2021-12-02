831132a79f05f6bb2400c70ac82548ce332e4dd2
magicjjfeng <1963487815@qq.com>
2021-12-02 17:27:15 +0800

    this is second

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,5 +1,8 @@
 package com.atguigu.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author FJJ
  * @creat 2021--12--02--17:21
@@ -8,5 +11,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        List<Object> objects = new ArrayList<>();
     }
 }

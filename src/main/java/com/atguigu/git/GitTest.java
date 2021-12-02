816c25175fec1396ebdd1653fd84505d2be00a32
magicjjfeng <1963487815@qq.com>
2021-12-02 19:00:01 +0800

    this is master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git4 master");
         List<Object> objects = new ArrayList<>();
     }
 }
